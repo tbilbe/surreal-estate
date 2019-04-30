22ca5feeb2430e9ec04d80773c2dbaa0f10d65e9
tbilbe <tbilbe@gmail.com>
2019-04-30 11:46:47 +0100

    updated css on add properties page

--- src/components/add-properties.jsx
+++ src/components/add-properties.jsx
@@ -25,51 +25,60 @@ class AddProperties extends React.Component {
   };
 
   render() {
-    const { title, type, city } = this.state.fields;
+    // const { title, type, city } = this.state.fields;
     return (
       <div className="addProperty">
-        <form onSubmit={this.handleAddProperty}>
-          <label htmlFor="title">
-            Title:
-            <input
-              name="title"
-              onChange={this.handleFieldChange}
-              value={this.state.fields.title}
-            />
-          </label>
-          <br />
-          <label>
-            Type:
-            <select
-              name="type"
-              onChange={this.handleFieldChange}
-              value={this.state.fields.type}
-            >
-              <option value="Flat">Flat</option>
-              <option value="Detached">Detached</option>
-              <option value="Semi-Detached">Semi-Detached</option>
-              <option value="Terraced">Terraced</option>
-              <option value="End of Terrace">End of Terrrace</option>
-              <option value="Cottage">Cottage</option>
-              <option value="Bungalow">Bungalow</option>
-            </select>
-          </label>
-          <br />
-          <label>
-            City:
-            <select
-              name="city"
-              onChange={this.handleFieldChange}
-              value={this.state.fields.city}
-            >
-              <option value="Manchester">Manchester</option>
-              <option value="Leeds">Leeds</option>
-              <option value="Sheffield">Sheffield</option>
-              <option value="Liverpool">Liverpool</option>
-            </select>
-          </label>
-          <br />
-          <button type="submit">Add</button>
+        <h3>
+          Please input the property details inside the form to add a property to
+          our database
+        </h3>
+        <form autocomplete="off" onSubmit={this.handleAddProperty}>
+          <div>
+            <label htmlFor="title">
+              Title:
+              <input
+                name="title"
+                onChange={this.handleFieldChange}
+                value={this.state.fields.title}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Type:
+              <select
+                name="type"
+                onChange={this.handleFieldChange}
+                value={this.state.fields.type}
+              >
+                <option value="Flat">Flat</option>
+                <option value="Detached">Detached</option>
+                <option value="Semi-Detached">Semi-Detached</option>
+                <option value="Terraced">Terraced</option>
+                <option value="End of Terrace">End of Terrrace</option>
+                <option value="Cottage">Cottage</option>
+                <option value="Bungalow">Bungalow</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <label>
+              City:
+              <select
+                name="city"
+                onChange={this.handleFieldChange}
+                value={this.state.fields.city}
+              >
+                <option value="Leeds">Leeds</option>
+                <option value="Liverpool">Liverpool</option>
+                <option value="Sheffield">Sheffield</option>
+                <option value="Manchester">Manchester</option>
+              </select>
+            </label>
+          </div>
+          <div className="submit-btn">
+            <button type="submit">Add</button>
+          </div>
         </form>
       </div>
     );
